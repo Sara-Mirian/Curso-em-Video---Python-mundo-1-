120fd74420c4f0d567dc72c19590b0ba3c159652
Sara-Mirian <118771054+Sara-Mirian@users.noreply.github.com>
2022-11-28 19:45:06 -0300

    Add files via upload

---
+++ Desafio 016.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia um número qualquer e mostre na tela a sua posição inteira
+
+from math import floor
+Num=float(input("Desafio 016\nDigite um número :\n"))
+print(f"Você digitou : {floor(Num)}")
